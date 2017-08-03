a68e4572f4304851726d4485a761569502b6ef70
Lawrence Hubbell <nathan@pellego.com>
2017-08-02 18:28:01 -0700

    switched to a different modal package, which got the code working

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import Vue from 'vue';
-import App from './app.vue';
-import { SweetModal, SweetModalTab } from 'sweet-modal-vue';
+import VModal from 'vue-js-modal'
+
+Vue.use(VModal)
 
 new Vue({
   el: '#myapp',
   components: {
-    SweetModal,
-    SweetModalTab,
-    App
   },
   data: {
     message: "Hello Vue"
   },
   methods: {
-    openModal: function() {
-        console.log(SweetModal);
-        this.$refs.modal.open();
+    show: function () {
+      this.$modal.show('hello-world');
+    },
+    hide: function () {
+      this.$modal.hide('hello-world');
     }
   }
 })
